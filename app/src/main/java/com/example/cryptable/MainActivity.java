eb3b7b0f5997d163a8e04663a1a62badaa02af6e
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2021-05-02 14:34:07 +0530

    Basic home done

--- app/src/main/java/com/example/cryptable/MainActivity.java
+++ app/src/main/java/com/example/cryptable/MainActivity.java
@@ -1,15 +1,37 @@
 package com.example.cryptable;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout imageEncrypt, textEncrypt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        imageEncrypt = findViewById(R.id.img_encrypt);
+        textEncrypt = findViewById(R.id.text_encrypt);
+
+        imageEncrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Image", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        textEncrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Text", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
