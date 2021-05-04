1b32f2ce8a27c9a4321070e7258647625bbd93ac
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2021-05-04 17:20:32 +0530

    Enc text functions written

--- app/src/main/java/com/example/cryptable/TextEncoder.java
+++ app/src/main/java/com/example/cryptable/TextEncoder.java
@@ -1,5 +1,6 @@
 package com.example.cryptable;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ClipData;
@@ -23,6 +24,12 @@ public class TextEncoder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_encoder);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setTitle("Text Encryption");
+        actionBar.show();
+
         enterText = findViewById(R.id.enter_text_editText);
         encryptedText = findViewById(R.id.tv_encrypted_txt);
         encryptBtn = findViewById(R.id.encrypt_btn);
