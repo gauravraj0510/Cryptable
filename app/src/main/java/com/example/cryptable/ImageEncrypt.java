fbb20e20d2909b1497dbd15f6e68f47d254cd12a
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2021-05-06 14:38:42 +0530

    Image encryption started

--- app/src/main/java/com/example/cryptable/ImageEncrypt.java
+++ app/src/main/java/com/example/cryptable/ImageEncrypt.java
@@ -1,11 +1,34 @@
 package com.example.cryptable;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 
 public class ImageEncrypt extends AppCompatActivity {
+
+    Button encryptBtn, decryptBtn;
+    String sImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +38,70 @@ public class ImageEncrypt extends AppCompatActivity {
         actionBar.setHomeButtonEnabled(true);
         actionBar.setTitle("Crypt Image");
         actionBar.show();
+
+        encryptBtn = findViewById(R.id.enc_img_btn);
+        decryptBtn = findViewById(R.id.dec_img_btn);
+
+        encryptBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ContextCompat.checkSelfPermission(ImageEncrypt.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ImageEncrypt.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+                }
+                else{
+                    selectImage();
+                }
+            }
+        });
+
+        decryptBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),100);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            selectImage();
+        }
+        else {
+            Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100 && resultCode==RESULT_OK && data!=null){
+            Uri uri = data.getData();
+            Bitmap bitmap;
+            ImageDecoder.Source source = null;
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+                source = ImageDecoder.createSource(this.getContentResolver(), uri);
+                try {
+                    bitmap = ImageDecoder.decodeBitmap(source);
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG,100, stream);
+                    byte[] bytes = stream.toByteArray();
+                    sImage = Base64.encodeToString(bytes, Base64.DEFAULT);
+                    Toast.makeText(this, sImage, Toast.LENGTH_SHORT).show();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
